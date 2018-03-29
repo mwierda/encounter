656735799b26f1d91f877795d509f45f39dba3d0
mwierda <wierda.mckenzie@gmail.com>
2018-03-29 11:41:52 -0500

    Change unarmed durability to UNBREAKABLE

--- src/main/kotlin/Enemy.kt
+++ src/main/kotlin/Enemy.kt
@@ -1,5 +1,5 @@
 open class Enemy(_title:String,_dangerLevel:DangerRating) {
     val title = _title
     val dangerLevel = _dangerLevel
-    open var weapon:Weapon = Weapon(WeaponType.UNARMED,Durability.WORN)
+    open var weapon:Weapon = Weapon(WeaponType.UNARMED,Durability.UNBREAKABLE)
 }
